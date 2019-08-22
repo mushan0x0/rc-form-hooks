d97ef7c1031a87c3ee69c27bcfecc497cfab9912
mushan0x0 <mushan0x0@gmail.com>
2019-08-22 10:48:47 +0800

    fix CI.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,13 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'react-app',
-    'plugin:prettier/recommended'
+    'react-app'
   ],
   plugins: ['@typescript-eslint', 'react'],
   rules: {
     '@typescript-eslint/indent': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-var-requires': 0,
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/camelcase': 0
   }
 };
